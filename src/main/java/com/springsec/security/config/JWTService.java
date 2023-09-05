1971b8a73a27642491c8673bbaaa2a800991fb69
Jemo <jamesshisia6@gmail.com>
2023-09-05 15:55:30 +0300

    Generate web token

--- src/main/java/com/springsec/security/config/JWTService.java
+++ src/main/java/com/springsec/security/config/JWTService.java
@@ -2,18 +2,24 @@ package com.springsec.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
     private static final String SECRET_KEY=
             "8F0A42D3B12F60D8E6C5D752DFE6A8A58E8A775570CB5DD5C172D2E69546D6D1";
     public String extractUsername(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken,Claims::getSubject);
     }
 
     public Claims extractAllClaims(String jwtToken){
@@ -28,5 +34,27 @@ public class JWTService {
     private Key getSignKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public <T> T extractClaim(String jwtToken , Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+
+    /**Generate Web Token without extra claims**/
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    /**Generate Web Token with claims**/
+    public String generateToken(Map<String,Object> extraClaims, UserDetails userDetails){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 }
